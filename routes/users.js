f1cb352be41e8372c821765b00c818cc3a2da12e
claudiaortiz1311 <claudia.ortiz@intive.com>
2022-10-22 04:02:02 -0300

    Cors

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,13 @@ var User = require('../models/user');
 const user = require('../models/user');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+const cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
+router.get('/',cors.corsWithOptions, authenticate.verifyUser,authenticate.verifyAdmin, (req, res, next) => {
   User.find({}, (err, users) => {
     if (err) {
       return next(err);
@@ -21,7 +22,7 @@ router.get('/', authenticate.verifyUser,authenticate.verifyAdmin, (req, res, nex
   })
 });
 
-router.post('/signup', (req,res,next) => {
+router.post('/signup', cors.corsWithOptions, (req,res,next) => {
   User.register(new User({username: req.body.username}),
   req.body.password, (err, user) => {
     if (err) {
@@ -51,14 +52,14 @@ router.post('/signup', (req,res,next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => { 
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => { 
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
-router.get('/logout', (req, res) => {
+router.get('/logout', cors.corsWithOptions, (req, res) => {
   if (req.session) {
     req.session.destroy();
     res.clearCookie('session-id');
